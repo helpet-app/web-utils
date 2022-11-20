59fce59ad62efb2fe7cc7b6e62f56f687543e5b0
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-20 15:31:15 +0700

    Add common page response

---
+++ src/main/java/com/helpet/web/dto/PageResponse.java
@@ -0,0 +1,36 @@
+package com.helpet.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+public class PageResponse<T> {
+    private int totalNumberOfPages;
+
+    private long totalNumberOfElements;
+
+    private int size;
+
+    private int number;
+
+    private int numberOfElements;
+
+    private List<T> content;
+
+    public PageResponse(Page<T> page) {
+        this.totalNumberOfPages = page.getTotalPages();
+        this.totalNumberOfElements = page.getTotalElements();
+        this.size = page.getSize();
+        this.number = page.getNumber();
+        this.numberOfElements = page.getNumberOfElements();
+        this.content = page.getContent();
+    }
+
+    public static <T> PageResponse<T> of(Page<T> page) {
+        return new PageResponse<>(page);
+    }
+}
