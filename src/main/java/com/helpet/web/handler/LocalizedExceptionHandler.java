7905f8343536b4b82f29d3e9b4f5b9db2469c0ba
Efim Nikitin <e.nikitin1@g.nsu.ru>
2022-11-21 17:41:19 +0700

    Change localized exception handler

--- src/main/java/com/helpet/web/handler/LocalizedExceptionHandler.java
+++ src/main/java/com/helpet/web/handler/LocalizedExceptionHandler.java
@@ -5,8 +5,8 @@ import com.helpet.web.response.ErrorBody;
 import com.helpet.web.response.UnsuccessfulResponseBody;
 import com.helpet.web.util.Localizer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Locale;
 
@@ -17,40 +17,42 @@ public class LocalizedExceptionHandler {
         this.localizer = localizer;
     }
 
-    protected UnsuccessfulResponseBody buildResponse(LocalizedException ex, Locale locale) {
+    protected ResponseEntity<UnsuccessfulResponseBody> buildResponseEntity(LocalizedException ex,
+                                                                           HttpStatus status,
+                                                                           Locale locale) {
         ErrorBody errorBody = new ErrorBody(ex.getCode(),
                                             localizer.l10n(ex.getExceptionKey(), locale),
                                             localizer.l10n(ex.getReasonKey(), locale));
-        return new UnsuccessfulResponseBody(errorBody);
+        return new ResponseEntity<>(new UnsuccessfulResponseBody(errorBody), status);
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadRequestLocalizedException.class)
-    protected UnsuccessfulResponseBody handleBadRequestLocalizedException(BadRequestLocalizedException ex, Locale locale) {
-        return buildResponse(ex, locale);
+    protected ResponseEntity<UnsuccessfulResponseBody> handleBadRequestLocalizedException(BadRequestLocalizedException ex,
+                                                                                          Locale locale) {
+        return buildResponseEntity(ex, HttpStatus.BAD_REQUEST, locale);
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(UnauthorizedLocalizedException.class)
-    protected UnsuccessfulResponseBody handleUnauthorizedLocalizedException(UnauthorizedLocalizedException ex, Locale locale) {
-        return buildResponse(ex, locale);
+    protected ResponseEntity<UnsuccessfulResponseBody> handleUnauthorizedLocalizedException(UnauthorizedLocalizedException ex,
+                                                                                            Locale locale) {
+        return buildResponseEntity(ex, HttpStatus.UNAUTHORIZED, locale);
     }
 
-    @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(ForbiddenLocalizedException.class)
-    protected UnsuccessfulResponseBody handleForbiddenLocalizedException(ForbiddenLocalizedException ex, Locale locale) {
-        return buildResponse(ex, locale);
+    protected ResponseEntity<UnsuccessfulResponseBody> handleForbiddenLocalizedException(ForbiddenLocalizedException ex,
+                                                                                         Locale locale) {
+        return buildResponseEntity(ex, HttpStatus.FORBIDDEN, locale);
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundLocalizedException.class)
-    protected UnsuccessfulResponseBody handleNotFoundLocalizedException(NotFoundLocalizedException ex, Locale locale) {
-        return buildResponse(ex, locale);
+    protected ResponseEntity<UnsuccessfulResponseBody> handleNotFoundLocalizedException(NotFoundLocalizedException ex,
+                                                                                        Locale locale) {
+        return buildResponseEntity(ex, HttpStatus.NOT_FOUND, locale);
     }
 
-    @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(ConflictLocalizedException.class)
-    protected UnsuccessfulResponseBody handleConflictLocalizedException(ConflictLocalizedException ex, Locale locale) {
-        return buildResponse(ex, locale);
+    protected ResponseEntity<UnsuccessfulResponseBody> handleConflictLocalizedException(ConflictLocalizedException ex,
+                                                                                        Locale locale) {
+        return buildResponseEntity(ex, HttpStatus.CONFLICT, locale);
     }
 }
