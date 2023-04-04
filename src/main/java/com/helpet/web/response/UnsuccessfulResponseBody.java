bf2bd4a4134d1bff6b2bebd5b915021c69e14ac7
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-04 15:25:31 +0700

    Replace error with errors

--- src/main/java/com/helpet/web/response/UnsuccessfulResponseBody.java
+++ src/main/java/com/helpet/web/response/UnsuccessfulResponseBody.java
@@ -2,12 +2,18 @@ package com.helpet.web.response;
 
 import lombok.Getter;
 
+import java.util.*;
+
 @Getter
 public class UnsuccessfulResponseBody extends ResponseBody {
-    private final ErrorBody error;
+    private final Set<ErrorBody> errors;
+
+    public UnsuccessfulResponseBody(Collection<ErrorBody> errors) {
+        super(false);
+        this.errors = new HashSet<>(errors);
+    }
 
     public UnsuccessfulResponseBody(ErrorBody error) {
-        super(false);
-        this.error = error;
+        this(List.of(error));
     }
 }
